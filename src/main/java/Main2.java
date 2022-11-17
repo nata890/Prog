9d8fc4c02e3be10e7d0d3652b774f053b38d8b11
Natalia Ceballos <nataliaceballos1703@gmail.com>
2022-11-17 10:32:11 -0500

    libreria

---
+++ src/main/java/Main2.java
@@ -0,0 +1,42 @@
+import javax.swing.*;
+import javax.xml.crypto.Data;
+import java.net.MalformedURLException;
+import java.nio.file.Paths;
+
+public class Main2 {
+
+    public static void main(String[] args) {
+
+        String windowsPath = "C:\\Users\\natal\\OneDrive\\Documentos\\Prueba.txt";
+
+        Data data = null;
+
+        String text = "Esto es un texto de prueba";
+
+        //Write text file
+        data = new Data(text);
+        try {
+            data.write(Paths.get(windowsPath).toUri().toURL(), true);
+        }
+        catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+
+        //Read text file
+        data = null;
+        try {
+            data = new Data(data.write(Paths.get(windowsPath).toUri().toURL(), true));
+        }
+        catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+
+        if (data!= null){
+            String newText = data.toText();
+            JOptionPane.showMessageDialog(null, newText);
+        }
+
+
+
+    }
+}
